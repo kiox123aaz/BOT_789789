2c9ac649ba9816eeb2a6bf4eca3724d5f7352446
poori99988 <79213511+poori99988@users.noreply.github.com>
2021-07-28 16:23:29 +0700

    Add files via upload

---
+++ include/play.js
@@ -0,0 +1,185 @@
+const Discord = require("discord.js");
+const ytdlDiscord = require("ytdl-core-discord");
+const scdl = require("soundcloud-downloader");
+const { canModifyQueue } = require("../util/neiUtill");
+
+module.exports = {
+  async play(song, message) {
+    const { PRUNING, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
+    const queue = message.client.queue.get(message.guild.id);
+
+    if (!song) {
+      queue.channel.leave();
+      message.client.queue.delete(message.guild.id);
+      return queue.textChannel.send("").catch(console.error);
+    }
+
+    let stream = null;
+
+    try {
+      if (song.url.includes("youtube.com")) {
+        stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
+      } else if (song.url.includes("soundcloud.com") && SOUNDCLOUD_CLIENT_ID) {
+        const info = await scdl.getInfo(song.url, SOUNDCLOUD_CLIENT_ID);
+        const opus = scdl.filterMedia(info.media.transcodings, { format: scdl.FORMATS.OPUS });
+        stream = await scdl.downloadFromURL(opus[0].url, SOUNDCLOUD_CLIENT_ID);
+      }
+    } catch (error) {
+      if (queue) {
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
+      }
+
+      console.error(error);
+      return message.channel.send(`Error: ${error.message ? error.message : error}`);
+    }
+
+    queue.connection.on("disconnect", () => message.client.queue.delete(message.guild.id));
+
+    const type = song.url.includes("youtube.com") ? "opus" : "ogg/opus";
+    const dispatcher = queue.connection
+      .play(stream, { type: type })
+      .on("finish", () => {
+        if (collector && !collector.ended) collector.stop();
+
+        if (queue.loop) {
+          // if loop is on, push the song back at the end of the queue
+          // so it can repeat endlessly
+          let lastSong = queue.songs.shift();
+          queue.songs.push(lastSong);
+          module.exports.play(queue.songs[0], message);
+        } else {
+          // Recursively play the next song
+          queue.songs.shift();
+          module.exports.play(queue.songs[0], message);
+        }
+      })
+      .on("error", (err) => {
+        console.error(err);
+        queue.songs.shift();
+        module.exports.play(queue.songs[0], message);
+      });
+    dispatcher.setVolumeLogarithmic(queue.volume / 100);
+
+    try {
+      var playingMessage = await queue.textChannel.send(new Discord.MessageEmbed()
+                                                        .setTitle(`🎶 **เริ่มเล่น:**`)
+                                                        .setDescription(`✅ [${song.title}](${song.url})🟢`)
+                                                        .setFooter(`เริ่มเล่นเเล้ว`)
+                                                        .setColor(`BLUE`));
+      await playingMessage.react("⏭");
+      await playingMessage.react("⏯");
+      await playingMessage.react("🔇");
+      await playingMessage.react("🔉");
+      await playingMessage.react("🔊");
+      await playingMessage.react("🔁");
+      await playingMessage.react("⏹");
+    } catch (error) {
+      console.error(error);
+    }
+
+    const filter = (reaction, user) => user.id !== message.client.user.id;
+    var collector = playingMessage.createReactionCollector(filter, {
+      time: song.duration > 0 ? song.duration * 1000 : 600000
+    });
+
+    collector.on("collect", (reaction, user) => {
+      if (!queue) return;
+      const member = message.guild.member(user);
+
+      switch (reaction.emoji.name) {
+        case "⏭":
+          queue.playing = true;
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.connection.dispatcher.end();
+          queue.textChannel.send(`${user} ⏩ ข้ามเพลงเเล้วครับ`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          collector.stop();
+          break;
+
+        case "⏯":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          if (queue.playing) {
+            queue.playing = !queue.playing;
+            queue.connection.dispatcher.pause(true);
+            queue.textChannel.send(`${user} ⏸ หยุดเพลงเเล้ว..`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          } else {
+            queue.playing = !queue.playing;
+            queue.connection.dispatcher.resume();
+            queue.textChannel.send(`${user} ▶ เริ่มเล่นเพลงต่อเเล้วครับ!`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          }
+          break;
+                  case "🔇":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return
+          if (queue.volume <= 0) {
+            queue.volume = 100;
+            queue.connection.dispatcher.setVolumeLogarithmic(100 / 100);
+            queue.textChannel.send(`${user} 🔊 เปิดเสียง!`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          } else {
+            queue.volume = 0
+            queue.connection.dispatcher.setVolumeLogarithmic(0);
+            queue.textChannel.send(`${user} 🔇 ปิดเสียง!`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          }
+          break;
+
+        case "🔉":
+          reaction.users.remove(user).catch(console.error);
+          if (queue.volume == 0) return;
+          if (!canModifyQueue(member)) return
+          if (queue.volume - 10 <= 0) queue.volume = 0;
+          else queue.volume = queue.volume - 10;
+          queue.connection.dispatcher.setVolumeLogarithmic(queue.volume / 100);
+          queue.textChannel
+            .send(`🔉 ลดเสียงเหลือ ${queue.volume}%`).then(msg => msg.delete({timeout:2000}))
+            .catch(console.error);
+          break;
+
+        case "🔊":
+          reaction.users.remove(user).catch(console.error);
+          if (queue.volume == 100) return;
+          if (!canModifyQueue(member)) return
+          if (queue.volume + 10 >= 100) queue.volume = 100;
+          else queue.volume = queue.volume + 10;
+          queue.connection.dispatcher.setVolumeLogarithmic(queue.volume / 100);
+          queue.textChannel
+            .send(`🔊 เพิ่มเสียงเหลือ ${queue.volume}%`).then(msg => msg.delete({timeout:2000}))
+            .catch(console.error);
+          break;
+
+        case "🔁":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.loop = !queue.loop;
+          queue.textChannel.send(`เล่นซ้ำ ${queue.loop ? "**เปิด**" : "**ปิด**"}`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          break;
+
+        case "⏹":
+          reaction.users.remove(user).catch(console.error);
+          if (!canModifyQueue(member)) return;
+          queue.songs = [];
+          queue.textChannel.send(`⏹ ปิดเพลงแล้วครับ!`).then(msg => msg.delete({timeout:2000})).catch(console.error);
+          try {
+            queue.connection.dispatcher.end();
+          } catch (error) {
+            console.error(error);
+            queue.channel.leave();
+          }
+          collector.stop();
+          break;
+
+        default:
+          reaction.users.remove(user).catch(console.error);
+          break;
+      }
+    });
+
+    collector.on("end", () => {
+      playingMessage.reactions.removeAll().catch(console.error);
+      if (PRUNING && playingMessage && !playingMessage.deleted) {
+        playingMessage.delete({ timeout: 3000 }).catch(console.error);
+      }
+    });
+  }
+};
